008303fe2012327f99b90fd6e732254fcc4ac291
Paul Jung <pbnjcub@gmail.com>
2022-10-22 10:23:50 -0400

    dog bar add

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import DogName from './DogName'
 
 function App() {
+
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+      fetch("http://localhost:3001/pups")
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+          setDogs(data)
+      })
+  }, [])
+
+  console.log(dogs)
+
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
-      <div id="dog-bar"></div>
+      <div id="dog-bar">{dogs.map(dog =><DogName key={dog.id} name={dog.name}/>)}</div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info"></div>
---
+++ src/components/DogName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DogName({name}) {
+    return (
+        <span id='dog-bar span'>
+            {name}
+        </span>
+    )
+}
+
+export default DogName
